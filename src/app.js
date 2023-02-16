ecd4016bc7e5c9b6dd5d11ab6a097bf410e23645
rodrigo25 <rodrigoperettisoles@gmail.com>
2023-02-16 11:35:12 -0300

    agrego productos

--- src/app.js
+++ src/app.js
@@ -3,45 +3,38 @@ import ProductManager from "./ProductManager"
 const manager = new ProductManager
 const app = express();
 
-
-
-
-
-
-
+const products = await manager.getProducts();
 
 // DEVUELVE LOS PRODUCTOS//
 app.get("/product",async(req,res)=>{
 
-const products = await manager.getProducts();
-
+  const products = await manager.getProducts();
 const {limit} = req.query
-//para que devuelva la cantidad de productos puede ser  produc.number??,
-if(limit === number){
-return res.send(products.number)
-}
+//para que devuelva la cantidad de productos que hay
+if(limit <= 10){
+return res.send(products.length)
+} 
 
-
-
-})
-
-
+});
 
 
 //DEVUELVE LOS PRODUCTOS POR ID
 app.get("/product/:id",async (req,res)=>{
 
-  const {id} = req.params;
+  const {id} =   req.params;
 
-const productId = products.find ((p)=> p.id=== id)
+const productId = await products.find ((p)=> p.id=== id)
+if(p.id === id){
+  res.send(productId)
+}else {
+  throw new error (`ese ID no existe en el registro de productos`)
+}
 
-res.send(productId)
-
-})
+});
 
 
 
 // ESCUCHA LA LLAMADA
 app.listen(8080,()=>{
     console.log(" el servidor esta escuchando el puerto 8080")
-})
+});
